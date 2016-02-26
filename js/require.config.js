3d421e3f8fecbf1d9956f2124aa1d77d5bb1152c
Dave Wecker <dbwz8@hotmail.com>
2016-02-25 16:16:34 -0800

    Need to build gh-pages

--- js/require.config.js
+++
@@ -1,7 +0,0 @@
-require.config({
-    paths: {
-        "jquery" : "//code.jquery.com/jquery-2.1.1.min",
-        "ko" : "//cdnjs.cloudflare.com/ajax/libs/knockout/3.1.0/knockout-min",
-        "moment" : "//cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.4/moment.min"
-    }
-});

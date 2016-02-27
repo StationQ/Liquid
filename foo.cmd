7b03e49c71a14dd011930d8b3af7e76c0ed35ca3
Dave Wecker <dbwz8@hotmail.com>
2016-02-26 16:18:31 -0800

    .

--- foo.cmd
+++ foo.cmd
@@ -1,7 +1,10 @@
 @echo off
 setlocal
 for /f %%a in (foo.files) do (
-    copy "%%a" foo.tmp
-    sed -e "s:\(\(href\|src\)=.\)[.][.]/[.][.]:\1{{ site.github.url }}:" < foo.tmp > "%%a"
+    rem echo #### %%a
+    copy "%%a" foo.tmp 2>NUL 1>NUL
+    rem sed -e "s:\(\(href\|src\)=.\)[.][.]/[.][.]:\1{{ site.github.url }}:" < foo.tmp > "%%a"
+    rem grep "\(\(href\|src\)=.\)/\([a-zA-Z]\)" foo.tmp
+    sed -e "s:\(\(href\|src\)=.\)\(/[a-zA-Z]\):\1{{ site.github.url }}\3:" < foo.tmp > "%%a"
 )
 endlocal

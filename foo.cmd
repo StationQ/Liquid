8bcf44eecd02b0e1e740a85cdae243d0483fcda9
Dave Wecker <dbwz8@hotmail.com>
2016-02-26 17:18:55 -0800

    Changed google analytics id

--- foo.cmd
+++ foo.cmd
@@ -4,7 +4,8 @@ for /f %%a in (foo.files) do (
     rem echo #### %%a
     copy "%%a" foo.tmp 2>NUL 1>NUL
     rem sed -e "s:\(\(href\|src\)=.\)[.][.]/[.][.]:\1{{ site.github.url }}:" < foo.tmp > "%%a"
-    rem grep "\(\(href\|src\)=.\)/\([a-zA-Z]\)" foo.tmp
-    sed -e "s:\(\(href\|src\)=.\)\(/[a-zA-Z]\):\1{{ site.github.url }}\3:" < foo.tmp > "%%a"
+    rem sed -e "s:\(\(href\|src\)=.\)\(/[a-zA-Z]\):\1{{ site.github.url }}\3:" < foo.tmp > "%%a"
+    sed -e "s/'UA-6[0-9]*-1'/'UA-73250818-1'/" < foo.tmp > "%%a"
+    rem grep "'UA-6[0-9]*-1'" foo.tmp
 )
 endlocal
